8fabb54d378ca847a3df72c6851e9ee146b05a64
Felix Klauke <felix@felix-klauke.de>
2020-09-08 17:12:27 +0200

    Add spigot plugin base

---
+++ susceptor-plugin/src/main/java/de/marmeladenoma/susceptor/SusceptorPlugin.java
@@ -0,0 +1,19 @@
+package de.marmeladenoma.susceptor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SusceptorPlugin extends JavaPlugin {
+  @Override
+  public void onLoad() {
+
+  }
+
+  @Override
+  public void onEnable() {
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
